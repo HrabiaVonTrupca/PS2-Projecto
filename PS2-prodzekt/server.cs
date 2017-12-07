614f21ab24c367cfa8feaf78a8d26d0ab1d9ce78
KonradG <konrad.gospo@gmail.com>
2017-12-07 20:08:08 +0100

    init repo

---
+++ PS2-prodzekt/server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Diagnostics;
+
+namespace PS2_prodzekt
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message, string conID)
+        {
+            string userID = Context.ConnectionId;
+
+            Clients.Client(conID).broadcastMessage(message);
+
+            Debug.WriteLine("User " + userID + "send message: " + message);
+
+        }
+
+        public override Task OnConnected()
+        {
+            var name = Context.ConnectionId;
+            Debug.WriteLine(name.ToString() + "  connected");
+
+            return base.OnConnected();
+        }
+    }
+}
